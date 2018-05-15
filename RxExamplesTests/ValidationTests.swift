b1036be4cfcfad7410a081c30a8d15b7eb8f2af1
Jimmy Bouker <james.bouker@gmail.com>
2018-05-14 22:43:09 -0400

    Added more tests to the FormValidationViewModel

--- RxExamplesTests/ValidationTests.swift
+++ RxExamplesTests/ValidationTests.swift
@@ -6,10 +6,7 @@
 //  Copyright © 2018 Jimmy Bouker. All rights reserved.
 //
 
-import Foundation
 import RxSwift
-import RxCocoa
-import RxBlocking
 import RxTest
 import XCTest
 @testable import RxExamples
@@ -27,7 +24,7 @@ class ValidationTests: XCTestCase {
 		scheduler = TestScheduler(initialClock: 0)
 	}
 
-	func testSimple() {
+	func testUsernameAndPasswordValidation() {
 
 		let username = scheduler.createHotObservable([
 			next(0, ""),
@@ -51,14 +48,30 @@ class ValidationTests: XCTestCase {
 			next(180, "password"),
 			next(190, "password1"),
 			next(200, "password1S")
-
 		]).asObservable()
+
 		let input = ValidationViewModel.Input(username: username, password: password)
 		let output = viewModel.transform(input: input)
 
 		let bothOk = scheduler.createObserver(Bool.self)
+		let userOk = scheduler.createObserver(Bool.self)
+		let passOk = scheduler.createObserver(Bool.self)
 		output.bothOk.bind(to: bothOk).disposed(by: bag)
+		output.usernameOk.bind(to: userOk).disposed(by: bag)
+		output.passOk.bind(to: passOk).disposed(by: bag)
+
 		scheduler.start()
+
+		let expectedUserOk = [
+			next(0, false),
+			next(10, false),
+			next(20, false),
+			next(30, false),
+			next(40, false),
+			next(50, false),
+			next(60, true),
+		]
+		XCTAssertEqual(userOk.events, expectedUserOk)
 
 		let expectedBothOk = [
 			next(0, false),
@@ -79,6 +92,22 @@ class ValidationTests: XCTestCase {
 			next(190, false),
 			next(200, true)
 		]
+
+		let expectedPassOk = [
+			next(0, false),
+			next(110, false),
+			next(120, false),
+			next(130, false),
+			next(140, false),
+			next(150, false),
+			next(160, false),
+			next(170, false),
+			next(180, false),
+			next(190, false),
+			next(200, true)
+		]
+		
 		XCTAssertEqual(bothOk.events, expectedBothOk)
+		XCTAssertEqual(passOk.events, expectedPassOk)
 	}
 }
